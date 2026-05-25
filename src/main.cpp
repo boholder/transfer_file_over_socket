14d401a32d5c114167cf8803c0c4265493eb06d6
boholder <bottleholder@anche.no>
2026-05-25 16:26:36 +0800

    feat: add -r -p -h options

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,111 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <filesystem>
 
-int main()
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+#include <spdlog/spdlog.h>
+
+#define APP_VERSION "0.1.0"
+#define APP_LINK "https://github.com/boholder/transfer_file_over_socket"
+
+// [26-10-31 23:46:59.678] shorten-level thread-id source-file-and-line: message
+// ref: https://github.com/gabime/spdlog/wiki/Custom-formatting
+#define LOG_PATTERN "%^[%C-%m-%d %T.%e] %L %-5t %-8!s:%-4#: %v%$"
+
+#define HELP_TEXT                                    \
+    "Usage: tfos [OPTIONS]\n\n"                      \
+    "OPTIONS:\n"                                     \
+    "  -h, --help      Show this help message\n"     \
+    "  -r, --root      Where the served files are\n" \
+    "  -p, --port      Will listen on this port\n"
+
+static std::string EMPTY;
+
+// options
+static std::filesystem::path root_dir;
+static int port;
+
+namespace
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    /**
+     * ref: https://stackoverflow.com/a/868894/23093084
+     *
+     * @author iain
+     */
+    class InputParser
+    {
+    public:
+        InputParser(const int& argc, char** argv)
+        {
+            for (int i = 1; i < argc; ++i)
+                this->tokens.emplace_back(argv[i]);
+        }
+
+        [[nodiscard]] const std::string& getCmdOption(const std::string& option) const
+        {
+            if (auto itr = std::ranges::find(this->tokens, option);
+                itr != this->tokens.end() && ++itr != this->tokens.end()) // NOLINT(*-inc-dec-in-conditions)
+            {
+                return *itr;
+            }
+            return EMPTY;
+        }
+
+        [[nodiscard]] bool cmdOptionExists(const std::string& option) const
+        {
+            return std::ranges::find(this->tokens, option) != this->tokens.end();
+        }
+
+    private:
+        std::vector<std::string> tokens;
+    };
+} // namespace
+
+static void parse_cmd_args(const int argc, char** argv)
+{
+    const InputParser input(argc, argv);
+    std::string option;
+
+#define OPTION_PASSED(SHORT, LONG) input.cmdOptionExists(SHORT) || input.cmdOptionExists(LONG)
+
+#define ASSIGN_OPTION(SHORT, LONG, VARIABLE, DEFAULT)                                          \
+    VARIABLE = DEFAULT;                                                                        \
+    if (OPTION_PASSED(SHORT, LONG))                                                            \
+    {                                                                                          \
+        option = input.getCmdOption(SHORT);                                                    \
+        if (option.empty())                                                                    \
+            option = input.getCmdOption(LONG);                                                 \
+        if (option.empty())                                                                    \
+        {                                                                                      \
+            SPDLOG_ERROR("option [{}] requires a value but not provided, exit", SHORT);        \
+            std::cerr << "option [" << (SHORT) << "] requires a value but not provided, exit"; \
+            exit(1); /*NOLINT(*-mt-unsafe)*/                                                   \
+        }                                                                                      \
+        VARIABLE = option;                                                                     \
+    }
+
+    if (OPTION_PASSED("-h", "--help"))
+    {
+        std::cout << HELP_TEXT;
+        exit(0); // NOLINT(*-mt-unsafe)
+    }
+
+    ASSIGN_OPTION("-r", "--root", root_dir, std::filesystem::current_path());
+
+    std::string port_str;
+    ASSIGN_OPTION("-p", "--port", port_str, "18180");
+    port = std::stoi(port_str);
+}
+
+int main(const int argc, char** argv) // NOLINT(*-exception-escape)
+{
+    spdlog::set_pattern(LOG_PATTERN);
+    spdlog::set_level(spdlog::level::debug);
+
+    parse_cmd_args(argc, argv);
+
+    SPDLOG_INFO("Successfully initialized");
+    SPDLOG_DEBUG("root: {}", root_dir.string());
+    SPDLOG_DEBUG("port: {}", port);
 }
